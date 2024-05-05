848d239f004022a353490a9bf7ab669a5d759389
Abdulhakim Kullow <abdulhakim.kullow@student.moringaschool.com>
2024-05-05 10:47:29 +0300

    completed lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,23 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  const testCases = [
+    { word: "racecar", expected: true },
+    { word: "car", expected: false },
+    { word: "RaCecar", expected: true },
+    { word: "race car", expected: null, errorExpected: true },
+    { word: "", expected: null, errorExpected: true }
+  ];
+
+  testCases.forEach(({ word, expected, errorExpected }) => {
+    it(`returns ${expected} for "${word}"`, () => {
+      if (errorExpected) {
+        expect(() => isPalindrome(word)).toThrow();
+      } else {
+        const result = isPalindrome(word);
+        expect(result).toBe(expected);
+      }
+    });
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+function reverseString(string) {
+    return string.split("").reverse().join("");
+  }
+  
+  export function isPalindrome(word) {
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error("Input must contain only alphabetic characters");
+    }
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === reverseString(lowerCaseWord);
+  }
+  
